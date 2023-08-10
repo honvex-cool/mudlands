cfdd4c72aa529643f4c5d5a5119542e73fa46cfa
fjasion <filip.jasionowicz@student.uj.edu.pl>
2023-08-10 15:32:41 +0200

    Minor content change

--- src/main/java/entities/passives/Sticks.java
+++ src/main/java/entities/passives/Sticks.java
@@ -1,4 +1,16 @@
 package entities.passives;
 
+import actions.ActionType;
+import entities.Mob;
+
 public class Sticks extends Passive{
+    @Override
+    public void react(ActionType actionType, Mob actor) {
+        return;
+    }
+
+    @Override
+    public boolean isActive() {
+        return false;
+    }
 }
