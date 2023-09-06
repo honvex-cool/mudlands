3bfa68249dde76a02160acfc6df6344f236116f0
kuba.was <jakub93.was@student.uj.edu.pl>
2023-09-06 10:49:19 +0200

    Add mud Items

---
+++ src/main/java/openable/items/tools/MudAxeItem.java
@@ -0,0 +1,35 @@
+package openable.items.tools;
+
+import entities.materials.Damage;
+import openable.inventory.Inventory;
+import openable.items.materials.GhostEssenceItem;
+import openable.items.materials.MudEssenceItem;
+import openable.items.materials.ZombieBloodItem;
+import utils.Pair;
+
+public class MudAxeItem extends AxeItem{
+    private final MudEssenceItem mud = new MudEssenceItem();
+    private final GhostEssenceItem ghost = new GhostEssenceItem();
+
+    private final ZombieBloodItem zombie = new ZombieBloodItem();
+    private final int requiredZombie = 3;
+    private final int requiredMud = 3;
+    private final int requiredGhost = 3;
+    public MudAxeItem(){
+        super();
+        name = "MudAxe";
+        damage = new Damage(35, 1, 20, 25);
+        durability = 250;
+        recipe.add(new Pair<>(mud, requiredMud));
+        recipe.add(new Pair<>(zombie, requiredZombie));
+        recipe.add(new Pair<>(ghost, requiredGhost));
+    }
+
+    @Override
+    public boolean craft(Inventory inventory) {
+        if(super.craft(inventory)) {
+            return tryCrafting(inventory, new MudAxeItem());
+        }
+        return false;
+    }
+}
---
+++ src/main/java/openable/items/tools/MudPickaxeItem.java
@@ -0,0 +1,35 @@
+package openable.items.tools;
+
+import entities.materials.Damage;
+import openable.inventory.Inventory;
+import openable.items.materials.GhostEssenceItem;
+import openable.items.materials.MudEssenceItem;
+import openable.items.materials.ZombieBloodItem;
+import utils.Pair;
+
+public class MudPickaxeItem extends PickaxeItem{
+    private final MudEssenceItem mud = new MudEssenceItem();
+    private final GhostEssenceItem ghost = new GhostEssenceItem();
+
+    private final ZombieBloodItem zombie = new ZombieBloodItem();
+    private final int requiredZombie = 3;
+    private final int requiredMud = 3;
+    private final int requiredGhost = 3;
+    public MudPickaxeItem(){
+        super();
+        name = "MudPickaxe";
+        damage = new Damage(1, 30, 20, 1);
+        durability = 250;
+        recipe.add(new Pair<>(mud, requiredMud));
+        recipe.add(new Pair<>(zombie, requiredZombie));
+        recipe.add(new Pair<>(ghost, requiredGhost));
+    }
+
+    @Override
+    public boolean craft(Inventory inventory) {
+        if(super.craft(inventory)) {
+            return tryCrafting(inventory, new MudPickaxeItem());
+        }
+        return false;
+    }
+}
